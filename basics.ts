f00b7304caf49eacb945e08cd95b692ddd343dc9
paulAlexSerban <paul.alex.serban@gmail.com>
2021-10-04 11:21:33 +0300

    u-ts_02 | added | basics

---
+++ basics.ts
@@ -0,0 +1,13 @@
+/**
+ * TASK
+ * Use `console.log()` to print "Hello, World!"" on a new line in the console, which is also known as stdout or standard output.
+ * Use `console.log()` to print the contents 
+ * @param { String } parameterVariable 
+ */
+
+function greeting(parameterVariable: string ) {
+  console.log(`Hello, World!`);
+  console.log(`${parameterVariable} is here, have no fear!!`);
+}
+
+greeting('TypeScript');
